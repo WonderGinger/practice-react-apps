b6dd11210f03e27a33e4bd9b0a51d711ad4657b3
Wonder Ginger <brandt.willems@gmail.com>
2019-06-10 18:05:06 -0700

    Time machine with a couple bugs

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -5,59 +5,87 @@ export class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null),
+      history: [{
+          squares:  Array(9).fill(null)
+      }],
       xIsNext: true,
       stepCount: 0,
     };
   }
 
   handleClick = index => {
-    let newSquares = this.state.squares.slice();
-    if (newSquares[index] || this.checkWinner()) {
+    let squares = this.state.history[this.state.stepCount].squares.slice(0, this.state.stepCount + 1);
+    if (squares[index] || checkWinner(squares)) {
       return;
     }
-    newSquares[index] = this.state.xIsNext ? 'X' : 'O';
-
+    squares[index] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
-      squares: newSquares,
+      history: [...this.state.history, { squares }],
       xIsNext: !this.state.xIsNext,
-      stepCount: this.state.stepCount + 1
+      stepCount: this.state.history.length
+    });
+  };
+
+  timeTravel = step => {
+    this.setState({
+      stepCount: step,
+      xIsNext: (step % 2) === 0
     });
   }
 
-  checkWinner = () => {
-    let squares = this.state.squares;
-    const winCons = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-    for (let con of winCons) {
-      let [a, b, c] = con;
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return true;
-      }
-    }
-    return false;
-  }
-
   render() {
+    const history = this.state.history;
+    const current = history[this.state.stepCount];
     return (
       <div style={styles.app}>
-        <Board squares={this.state.squares} onClick={index => this.handleClick(index)}/>
+        <Board 
+          squares={current.squares} 
+          onClick={index => this.handleClick(index)}
+        />
+
+        <div style={styles.gameInfo}>
+          {this.state.history.map(({square}, step) => (
+            <button key={step} onClick={() => this.timeTravel(step)}>
+                Turn {step}
+            </button>
+          ))}
+        </div>
       </div>
     )
   }
 }
 
+const checkWinner = squares => {
+  const winCons = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let con of winCons) {
+    let [a, b, c] = con;
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return true;
+    }
+  }
+  return false;
+}
+
 const styles = {
   app: {
-    margin: 100
+    display: 'flex',
+    margin: 100,
+  },
+  gameInfo: {
+    flexGrow: .2,
+    display: 'flex',
+    flexDirection: 'column',
+    margin: 5,
+    border: "1px solid blue",
   }
 }
 
